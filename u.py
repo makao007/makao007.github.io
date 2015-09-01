a43be5f2609fa44fccad8e2b7acbece376107514
makao007 <makao007@126.com>
2015-09-01 12:29:59 +0800

    git update

--- u.py
+++ u.py
@@ -1,16 +1,20 @@
-#!/usr/local/bin python
+#!/usr/local/bin/python
 #encoding:utf8
 import os
 import sys
 
 def update_git (msg):
     print 'try to push to github'
-    site_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_site')
+    site_path = os.path.dirname(os.path.abspath(__file__))
+
     os.chdir(site_path)
-    cmds = ['ls',  'git add --all', 'git commit -m "%s"' %msg, 'git push -u origin master']
+    print os.popen('jekyll build').read()
+
+    os.chdir('_site')
+    cmds = ['git add --all', 'git commit -m "%s"' %msg, 'git push -u origin master']
 
     for i in cmds:
-      print os.popen(i).read()
+        print os.popen(i).read()
       
 
 if __name__ == "__main__":
